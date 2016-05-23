08bfdc10c1d27a5e0835e938fa878d63a1aa0490
Николаев Виктор Анатольевич <ip511s14@awp06.csc.local>
2016-05-23 19:23:16 +0600

    Edit ur.c

--- test/test_ur.c
+++ test/test_ur.c
@@ -1,7 +1,7 @@
 #include "ur.h"
 #include "ctest.h"
 
-CTEST(quad_suite, test_quad_equation) 
+CTEST(quad_suite, test_quad_equation1) 
 {
 	const double a = 4, b = 16, c = 12;
 
@@ -16,4 +16,17 @@ CTEST(quad_suite, test_quad_equation)
 	ASSERT_DBL_NEAR(expected_x2, x2);
 }
 
+CTEST(quad_suite, test_quad_equation2) 
+{
+	const double a = 8, b = 32, c = 24;
 
+        double x1, x2;
+
+ 	urav (a, b, c, &x1, &x2);
+ 
+	const double expected_x1 = -3;
+	const double expected_x2 =  -1;
+
+	ASSERT_DBL_NEAR(expected_x1, x1);
+	ASSERT_DBL_NEAR(expected_x2, x2);
+}
